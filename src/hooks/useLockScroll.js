871760457b7a57848927ebd09188aecb2881cc70
Valuh-Marko <marko.valuh@gmail.com>
2025-07-22 12:34:51 +0200

    fix: First visit scroll lock fix on mobile

--- src/hooks/useLockScroll.js
+++ src/hooks/useLockScroll.js
@@ -2,16 +2,14 @@ import { useEffect } from "react";
 
 export const useScrollLock = (lock) => {
   useEffect(() => {
-    const originalOverflow = document.body.style.overflow;
-
     if (lock) {
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = originalOverflow;
+      document.body.style.overflow = "auto";
     }
 
     return () => {
-      document.body.style.overflow = originalOverflow;
+      document.body.style.overflow = "auto";
     };
   }, [lock]);
 };
